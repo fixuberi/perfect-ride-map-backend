188d72e9ee8ff104e6bc13c4ef0c27cfab97d244
Yakov Zhavoronkov <yakov.zhavoronkov@jelvix.com>
2023-06-08 14:11:49 +0300

    scafold app, scafold Ride resource

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RideModule } from './ride/ride.module';
+
+@Module({
+  imports: [RideModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
